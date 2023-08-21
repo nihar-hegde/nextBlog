e3912e46ccb2d6f1b43caee2490f2ee1cb4a99a0
Nihar Hegde <niharhegde163@gmail.com>
2023-08-21 22:15:38 +0530

    minor changes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,12 +5,14 @@ const Navbar = () => {
   return (
     <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
       <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
-        <Link
-          href={"/"}
-          className="text-white/90 no-underline hover:text-white"
-        >
-          Nihar Hegde
-        </Link>
+        <h1 className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
+          <Link
+            href={"/"}
+            className="text-white/90 no-underline hover:text-white"
+          >
+            Nihar Hegde
+          </Link>
+        </h1>
       </div>
     </nav>
   );
