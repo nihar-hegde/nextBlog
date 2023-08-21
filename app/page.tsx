8b6e131a20625f2dad8cf3b5bf2c0b2b098c4306
Nihar Hegde <niharhegde163@gmail.com>
2023-08-21 22:12:09 +0530

    added profile pic

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ export default function Home() {
   return (
     <main className="px-6 mx-auto">
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
-        Hello and Welcome 👋$nbsp;
+        Hello and Welcome 👋&nbsp;
         <span className="whitespace-nowrap">
-          I am <span className="font-bold">Nihar Hegde</span>.
+          I&apos;m <span className="font-bold">Dave</span>.
         </span>
       </p>
     </main>
